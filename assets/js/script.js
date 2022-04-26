0e892a55cf5f8269f082799af88b7d4e236202c4
Adrian Strozzi <adrianstrozzi@gmail.com>
2022-04-25 21:18:47 -0500

    Fixed modal position and added styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,22 @@ let closeBtn = document.getElementById("closeSurveyBtn")
 let nextBtn = document.getElementById("nextSurveyBtn")
 
 
+// Open modal with Get started button
+
 getStartedBtn.onclick = function () {
-  surveyModal.style.display = "block";
+  surveyModal.style.display = "flex";
 }
 
+// Closes modal with Close button
 
 closeBtn.onclick = function () {
   surveyModal.style.display = "none";
+}
+
+// Closes modal while clicking outside it
+
+window.onclick = function (event) {
+  if (event.target == surveyModal) {
+    surveyModal.style.display = "none";
+  }
 }
